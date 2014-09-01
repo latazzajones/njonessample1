5c42916e8502db5cf2fccb588a3f4df9ef0c2673
Natasha Jones <tasha.k.jones@gmail.com>
2014-08-31 20:21:23 -0400

    first commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,20 @@
+class StaticPagesController < ApplicationController
+  
+  def index
+  end
+
+  def about 
+  end
+
+  def contact
+  end
+
+  def projects
+  end
+
+  def research
+  end
+
+  def publications
+  end
+end
