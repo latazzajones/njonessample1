2cd02591770f70c36a1ace546a9285a041db733f
Natasha Jones <tasha.k.jones@gmail.com>
2014-09-10 19:28:25 -0400

    updated media query css

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,7 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( nemestudio.css )
+Rails.application.config.assets.precompile += %w( nemestudio_media.css )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
